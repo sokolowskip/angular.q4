8d72939a20bc2d6f4c001062d084fd43a159b03c
Pawel Sokolowski <pa.sokolowski@gmail.com>
2013-11-05 20:45:22 +0100

    Pusty projekt w VS2012

---
+++ src/Q4.Angular/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace Q4.Angular.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ src/Q4.Angular/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Q4.Angular.WebApiApplication" Language="C#" %>
