7b5354eeb674ca429a771b28b5b6774fd6368917
unknown <psokolowski@psokolowski-nb.software.altkom.pl>
2013-11-06 12:48:52 +0100

    Integracja AngularJS z projektem ASP MVC w VS2012

---
+++ src/Q4.Angular/Content/app/js/app.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+var q4AngularApp = angular.module('q4AngularApp', [
+    'ngRoute',
+    'q4AngularControllers']);
+
+q4AngularApp.config([
+    '$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/developers', {
+                templateUrl: 'Content/app/partials/developers-list.html',
+                controller: 'developersCtrl'
+            })
+            .when('/projects', {
+                templateUrl: 'Content/app/partials/projects-list.html',
+                controller: 'projectsCtrl'
+            })
+            .otherwise({
+                redirectTo: '/developers'
+            });
+    }
+]);
---
+++ src/Q4.Angular/Content/app/js/controllers.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+var q4AngularControllers = angular.module('q4AngularControllers', []);
+
+q4AngularControllers.controller('developersCtrl', function developersCtrl($scope) {
+    $scope.ctrlName = 'Developers controller.';
+});
+
+q4AngularControllers.controller('projectsCtrl', function projectsCtrl($scope) {
+    $scope.ctrlName = 'Projects controller.';
+});
